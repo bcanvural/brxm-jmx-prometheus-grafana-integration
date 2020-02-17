ec4ebba3f0b7d70db7e0db3a7f13e33be3fa8e8d
Baris Can Vural <bcanvural@gmail.com>
2020-02-17 10:57:16 +0100

    Initial commit

---
+++ site/components/src/main/java/org/example/beans/Blogpost.java
@@ -0,0 +1,84 @@
+package org.example.beans;
+/*
+ * Copyright 2014-2019 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import java.util.Calendar;
+import java.util.List;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+import org.onehippo.cms7.essentials.components.model.Authors;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
+@HippoEssentialsGenerated(internalName = "myproject:blogpost")
+@Node(jcrType = "myproject:blogpost")
+public class Blogpost extends HippoDocument implements Authors {
+
+    public static final String TITLE = "myproject:title";
+    public static final String INTRODUCTION = "myproject:introduction";
+    public static final String CONTENT = "myproject:content";
+    public static final String PUBLICATION_DATE = "myproject:publicationdate";
+    public static final String CATEGORIES = "myproject:categories";
+    public static final String AUTHOR = "myproject:author";
+    public static final String AUTHOR_NAMES = "myproject:authornames";
+    public static final String LINK = "myproject:link";
+    public static final String AUTHORS = "myproject:authors";
+    public static final String TAGS = "hippostd:tags";
+
+   @HippoEssentialsGenerated(internalName = "myproject:publicationdate")
+    public Calendar getPublicationDate() {
+        return getSingleProperty(PUBLICATION_DATE);
+    }
+
+    @HippoEssentialsGenerated(internalName = "myproject:authornames")
+    public String[] getAuthorNames() {
+        return getMultipleProperty(AUTHOR_NAMES);
+    }
+
+    public String getAuthor() {
+        final String[] authorNames = getAuthorNames();
+        if(authorNames !=null && authorNames.length > 0){
+            return authorNames[0];
+        }
+        return null;
+    }
+
+    @HippoEssentialsGenerated(internalName = "myproject:title")
+    public String getTitle() {
+        return getSingleProperty(TITLE);
+    }
+
+    @HippoEssentialsGenerated(internalName = "myproject:content")
+    public HippoHtml getContent() {
+        return getHippoHtml(CONTENT);
+    }
+
+    @HippoEssentialsGenerated(internalName = "myproject:introduction")
+    public String getIntroduction() {
+        return getSingleProperty(INTRODUCTION);
+    }
+
+    @HippoEssentialsGenerated(internalName = "myproject:categories")
+    public String[] getCategories() {
+        return getMultipleProperty(CATEGORIES);
+    }
+
+    @Override
+    @HippoEssentialsGenerated(internalName = "myproject:authors")
+    public List<Author> getAuthors() {
+        return getLinkedBeans(AUTHORS, Author.class);
+    }
+}
